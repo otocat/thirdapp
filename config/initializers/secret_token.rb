f02e4c951b0dc0ed747934ac6fe42c06cc08ccab
otocat <otocat@yahoo.com>
2012-10-08 02:09:24 +0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thirdapp::Application.config.secret_token = '8872f7400193e875aefe05eb9f4ace5fa0fe87a0e40ceb92a4fa7db94d42c807109e7380044b6f58c1f30cd4962abbd696e9faa4aa996b7214cbaea225e03852'
